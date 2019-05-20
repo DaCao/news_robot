f2fb405cdb4d71a0885b2e12ca190dfb9350b431
Da Cao <da.cao@zoomiinc.com>
2019-05-19 23:21:00 -0400

    initial

---
+++ mailer.py
@@ -0,0 +1,45 @@
+import smtplib
+from email.mime.text import MIMEText
+
+
+class Mailer(object):
+
+    def __init__(self):
+
+        self.host = 'smtp.gmail.com'
+        self.port = 465
+        self.sender = 'phillycheesesteak432@gmail.com'
+        self.receiver = 'caoda0711@gmail.com'
+
+
+    def create_message(self):
+        message = MIMEText('content', 'plain', 'utf-8')
+        message['Subject'] = 'test'
+        message['From'] = self.sender
+        message['To'] = self.receiver
+        return message
+
+    def send(self):
+        try:
+            server_ssl = smtplib.SMTP_SSL(self.host, self.port)
+            # server_ssl.ehlo()
+            # server_ssl.starttls()
+            server_ssl.ehlo()
+            server_ssl.login(self.sender, 'pass-word')
+
+            message = self.create_message()
+            server_ssl.sendmail(self.sender, self.receiver, message.as_string())
+            server_ssl.close()
+            print('success')
+        except Exception as e:
+            print('Failed !!! ')
+            print(e)
+
+
+
+
+
+if __name__ == '__main__':
+
+    mailer = Mailer()
+    mailer.send()
